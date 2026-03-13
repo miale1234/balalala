a91d00eac58a73d4c302808fedf849cf55a2cbda
miale1234 <mihaiilade00@gmail.com>
2026-03-12 23:37:15 -0700

    Update index.html

--- index.php
+++ index.php
@@ -1,29 +1,17 @@
-<?php include 'db.php'; ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href="style.css">
-    <title>Agenda</title>
+<title>Agenda Telefonică</title>
+<link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <div class="container">
-        <h2>Agenda Mea</h2>
-        <a href="add.php" class="btn">Adaugă Contact</a>
-        <table>
-            <tr><th>Nume</th><th>Tel</th><th>Acțiuni</th></tr>
-            <?php
-            $res = $conn->query("SELECT * FROM agenda");
-            while($row = $res->fetch_assoc()): ?>
-            <tr>
-                <td><?= $row['nume'] ?></td>
-                <td><?= $row['telefon'] ?></td>
-                <td>
-                    <a href="edit.php?id=<?= $row['id'] ?>">Edit</a> | 
-                    <a href="delete.php?id=<?= $row['id'] ?>" style="color:red">Șterge</a>
-                </td>
-            </tr>
-            <?php endwhile; ?>
-        </table>
-    </div>
+
+<h1>Agenda Telefonică</h1>
+
+<a href="view.php">Vezi Contactele</a>
+<br><br>
+<a href="add.php">Adaugă Contact</a>
+
 </body>
 </html>
